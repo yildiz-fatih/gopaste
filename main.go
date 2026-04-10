951c62bdcae359030b4dd5fe1f3bc94c058c753d
Fatih <122681611+yildiz-fatih@users.noreply.github.com>
2026-04-10 12:54:58 +0300

    add basic HTTP server and home handler

--- main.go
+++ main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	host := "0.0.0.0"
+	port := 8080
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /{$}", handleHome)
+
+	log.Printf("Server running on port %d...\n", port)
+	err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), mux)
+	log.Fatal(err)
+}
+
+func handleHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello GoPaste"))
+}

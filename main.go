d67ab44ca33c261a8d135eaf3cfa53f35b3b4e09
Fatih <122681611+yildiz-fatih@users.noreply.github.com>
2026-04-12 01:16:59 +0300

    refactor routes

--- main.go
+++ main.go
@@ -23,16 +23,8 @@ func main() {
 		logger: slog.New(slog.NewTextHandler(os.Stdout, nil)),
 	}
 
-	mux := http.NewServeMux()
-
-	mux.Handle("GET /static/", http.StripPrefix("/static", http.FileServer(http.Dir("./static"))))
-	mux.HandleFunc("GET /{$}", app.handleHome)
-	mux.HandleFunc("GET /paste/{id}", app.handlePasteView)
-	mux.HandleFunc("POST /paste", app.handlePasteCreate)
-	mux.HandleFunc("GET /help", app.handleHelp)
-
 	app.logger.Info("Starting server", "host", host, "port", port)
-	err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), mux) // err is always non-nil
+	err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), app.newRouter()) // err is always non-nil
 	if err != nil {
 		app.logger.Error(err.Error())
 		os.Exit(1)
---
+++ router.go
@@ -0,0 +1,15 @@
+package main
+
+import "net/http"
+
+func (app *application) newRouter() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	mux.Handle("GET /static/", http.StripPrefix("/static", http.FileServer(http.Dir("./static"))))
+	mux.HandleFunc("GET /{$}", app.handleHome)
+	mux.HandleFunc("GET /paste/{id}", app.handlePasteView)
+	mux.HandleFunc("POST /paste", app.handlePasteCreate)
+	mux.HandleFunc("GET /help", app.handleHelp)
+
+	return mux
+}

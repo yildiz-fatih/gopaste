6d948ddfe0e4a5e7d99ae4f5497123f77bddf733
Fatih <122681611+yildiz-fatih@users.noreply.github.com>
2026-04-12 18:49:47 +0300

    add postgres connection with pgx

--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/joho/godotenv"
 )
 
 type application struct {
 	logger *slog.Logger
+	db     *sql.DB
 }
 
 func main() {
@@ -19,14 +24,39 @@ func main() {
 	flag.IntVar(&port, "port", 8080, "Port to listen on")
 	flag.Parse()
 
-	app := &application{
-		logger: slog.New(slog.NewTextHandler(os.Stdout, nil)),
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	_ = godotenv.Load()
+
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		logger.Error("DATABASE_URL environment variable is not set")
+		os.Exit(1)
 	}
 
-	app.logger.Info("Starting server", "host", host, "port", port)
-	err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), app.newRouter()) // err is always non-nil
+	db, err := sql.Open("pgx", dbURL)
 	if err != nil {
-		app.logger.Error(err.Error())
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	logger.Info("Connected to the database")
+
+	app := &application{
+		logger: logger,
+		db:     db,
+	}
+
+	logger.Info("Starting server", "host", host, "port", port)
+	err = http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), app.newRouter()) // err is always non-nil
+	if err != nil {
+		logger.Error(err.Error())
 		os.Exit(1)
 	}
 }

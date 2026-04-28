b09f66537cb6218b6826671d053004faa828fb40
Fatih <122681611+yildiz-fatih@users.noreply.github.com>
2026-04-28 16:04:30 +0300

    add migrations

---
+++ migrations/000001_init.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS pastes (
+    id SERIAL PRIMARY KEY,
+    slug TEXT UNIQUE NOT NULL,
+    content TEXT NOT NULL,
+    created TIMESTAMPTZ NOT NULL,
+    expires TIMESTAMPTZ NOT NULL
+);

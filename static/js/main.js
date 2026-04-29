d1f44e38eda7d3d991a4a236b3569fbb5a84b7c2
Fatih <122681611+yildiz-fatih@users.noreply.github.com>
2026-04-29 05:49:51 +0300

    polish UI layout, add tab support for paste editor

--- static/js/main.js
+++ static/js/main.js
@@ -11,3 +11,27 @@ async function copyContent() {
         alert("Failed to copy content: " + err)
     }
 }
+
+const pasteURL = document.querySelector(".paste-url")
+if (pasteURL) {
+    const original = pasteURL.innerText
+    pasteURL.addEventListener("mouseenter", () => pasteURL.innerHTML = "<em>click to copy</em>")
+    pasteURL.addEventListener("mouseleave", () => pasteURL.innerText = original)
+    pasteURL.addEventListener("click", () => navigator.clipboard.writeText(original))
+}
+
+const textArea = document.querySelector('textarea')
+if (textArea) {
+    textArea.addEventListener('keydown', (e) => {
+        if (e.key === 'Tab') {
+            // prevent default focus-jump behavior
+            e.preventDefault()
+            const cursorStart = textArea.selectionStart
+            const cursorEnd = textArea.selectionEnd
+            // insert 4 spaces at cursor position
+            textArea.value = textArea.value.substring(0, cursorStart) + '    ' + textArea.value.substring(cursorEnd)
+            // move cursor to after the inserted spaces
+            textArea.selectionStart = textArea.selectionEnd = cursorStart + 4
+        }
+    })
+}

f17eb49fbb941813797a39d09e28f3cd872f78d0
Fatih <122681611+yildiz-fatih@users.noreply.github.com>
2026-05-01 16:21:45 +0300

    add caching to GET paste endpoint

--- handlers.go
+++ handlers.go
@@ -8,6 +8,7 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/redis/go-redis/v9"
 	"github.com/yildiz-fatih/gopaste/internal/models"
 )
 
@@ -18,14 +19,46 @@ func (app *application) handleHome(w http.ResponseWriter, r *http.Request) {
 func (app *application) handlePasteView(w http.ResponseWriter, r *http.Request) {
 	slug := r.PathValue("slug")
 
-	p, err := app.pasteModel.Get(slug)
-	if err != nil {
-		if errors.Is(err, models.ErrNotFound) {
-			http.NotFound(w, r)
-			return
+	cacheHit := false
+	var p models.Paste
+
+	// check redis cache first
+	cachedPaste, err := app.redisClient.Get(r.Context(), fmt.Sprintf("paste:%s", slug)).Result()
+	if err != nil && err != redis.Nil {
+		// something went wrong with redis, and it's not a cache miss
+		app.logger.Error("redis get error", "error", err)
+	} else if err == nil {
+		// cache hit!
+		err = json.Unmarshal([]byte(cachedPaste), &p)
+		if err != nil {
+			app.logger.Error("redis unmarshal error", "error", err)
 		} else {
-			app.writeServerError(w, err)
-			return
+			cacheHit = true
+		}
+	}
+
+	if !cacheHit {
+		// get from database
+		p, err = app.pasteModel.Get(slug)
+		if err != nil {
+			if errors.Is(err, models.ErrNotFound) {
+				http.NotFound(w, r)
+				return
+			} else {
+				app.writeServerError(w, err)
+				return
+			}
+		}
+
+		// write to redis cache
+		pasteJson, err := json.Marshal(p)
+		if err != nil {
+			app.logger.Error("redis marshal error", "error", err)
+		} else {
+			err = app.redisClient.Set(r.Context(), fmt.Sprintf("paste:%s", p.Slug), pasteJson, time.Until(p.Expires)).Err()
+			if err != nil {
+				app.logger.Error("redis set error", "error", err)
+			}
 		}
 	}
 
@@ -62,13 +95,12 @@ func (app *application) handlePasteCreate(w http.ResponseWriter, r *http.Request
 	// write to redis
 	pasteJson, err := json.Marshal(paste)
 	if err != nil {
-		app.writeServerError(w, err)
-		return
-	}
-
-	err = app.redisClient.Set(r.Context(), fmt.Sprintf("paste:%s", paste.Slug), pasteJson, time.Until(paste.Expires)).Err()
-	if err != nil {
-		app.logger.Error("Failed to cache paste in redis", "error", err)
+		app.logger.Error("redis marshal error", "error", err)
+	} else {
+		err = app.redisClient.Set(r.Context(), fmt.Sprintf("paste:%s", paste.Slug), pasteJson, time.Until(paste.Expires)).Err()
+		if err != nil {
+			app.logger.Error("redis set error", "error", err)
+		}
 	}
 
 	http.Redirect(w, r, fmt.Sprintf("/paste/%s", paste.Slug), http.StatusSeeOther)

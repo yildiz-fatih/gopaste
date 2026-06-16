a4644926b668d8242d25616141865cc6fc5b8489
Fatih <122681611+yildiz-fatih@users.noreply.github.com>
2026-06-16 18:59:18 +0300

    add integration tests for PasteModel.Get

--- internal/models/paste_test.go
+++ internal/models/paste_test.go
@@ -1,9 +1,19 @@
 package models
 
 import (
+	"context"
+	"database/sql"
 	"errors"
+	"path/filepath"
+	"reflect"
 	"strings"
 	"testing"
+	"time"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/postgres"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 func TestRandomSlug(t *testing.T) {
@@ -46,6 +56,116 @@ func TestRandomSlug(t *testing.T) {
 			if !strings.ContainsRune(allowedChars, char) {
 				t.Errorf("contains disallowed character %q", char)
 			}
+		}
+	})
+}
+
+func newTestDB(t *testing.T) *sql.DB {
+	t.Helper()
+
+	// set up
+	ctx := context.Background()
+
+	migrationFiles, err := filepath.Glob(filepath.Join("..", "..", "migrations", "*.up.sql"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(migrationFiles) == 0 {
+		t.Fatal("no migration files found")
+	}
+
+	pgContainer, err := postgres.Run(ctx, "postgres:18.3-alpine",
+		postgres.WithInitScripts(migrationFiles...),
+		postgres.WithDatabase("test-db"),
+		postgres.WithUsername("postgres"),
+		postgres.WithPassword("postgres"),
+		testcontainers.WithWaitStrategy(
+			wait.ForLog("database system is ready to accept connections").
+				WithOccurrence(2).WithStartupTimeout(5*time.Second)),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Cleanup(func() {
+		if err := pgContainer.Terminate(ctx); err != nil {
+			t.Fatalf("failed to terminate pgContainer: %s", err)
+		}
+	})
+
+	connStr, err := pgContainer.ConnectionString(ctx, "sslmode=disable")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// connect
+	db, err := sql.Open("pgx", connStr)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Cleanup(func() {
+		db.Close()
+	})
+
+	return db
+}
+
+func TestPasteModel_Get(t *testing.T) {
+	t.Run("paste exists", func(t *testing.T) {
+		// set up a test database
+		db := newTestDB(t)
+
+		// insert a paste
+		query := `INSERT INTO pastes (slug, content, created, expires)
+	VALUES ($1, $2, NOW(), NOW() + $3 * INTERVAL '1 hour')
+	RETURNING *`
+		var paste Paste
+		slug := "test-slug"
+		err := db.QueryRow(query, slug, "test content", 1).Scan(&paste.ID, &paste.Slug, &paste.Content, &paste.Created, &paste.Expires)
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+
+		// call Get and check the return
+		pasteModel := PasteModel{DB: db}
+		got, err := pasteModel.Get(slug)
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+
+		if !reflect.DeepEqual(paste, got) {
+			t.Errorf("want %+v, got %+v", paste, got)
+		}
+	})
+	t.Run("paste does not exist", func(t *testing.T) {
+		// set up a test database
+		db := newTestDB(t)
+
+		// call Get and check the return
+		pasteModel := PasteModel{DB: db}
+		_, got := pasteModel.Get("doesnotmatterwhatiputhere")
+		if !errors.Is(got, ErrNotFound) {
+			t.Errorf("want %v, got %v", ErrNotFound, got)
+		}
+	})
+	t.Run("paste expired", func(t *testing.T) {
+		// set up a test database
+		db := newTestDB(t)
+
+		// insert a paste
+		query := `INSERT INTO pastes (slug, content, created, expires)
+		VALUES ($1, $2, NOW(), NOW() + $3 * INTERVAL '1 hour')`
+		slug := "test-slug"
+		_, err := db.Exec(query, slug, "test content", -1)
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+
+		// call Get and check the return
+		pasteModel := PasteModel{DB: db}
+		_, got := pasteModel.Get(slug)
+		if !errors.Is(got, ErrNotFound) {
+			t.Errorf("want %v, got %v", ErrNotFound, got)
 		}
 	})
 }
